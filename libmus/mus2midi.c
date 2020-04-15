837a779b53619591eb57c3a8d5c5213ba57ca542
resttime <resttime@users.noreply.github.com>
2020-04-15 00:29:57 -0400

    Refactor and handle all error cases

--- libmus/mus2midi.c
+++ libmus/mus2midi.c
@@ -6,45 +6,82 @@
 
 typedef struct {
     char sig[4];
-    uint16_t len;
+    uint16_t size;
     uint16_t offset;
-    uint16_t pchannels;
-    uint16_t schannels;
-    uint16_t numinstruments;
-    uint16_t reserved;
-} musheader_t;
+    uint16_t p_chnls;
+    uint16_t s_chnls;
+    uint16_t num_instr;
+    uint16_t resrvd;
+    uint16_t *instr;
+    void *data;
+} mus_t;
+
+mus_t* mus_load(char *file) {
+    // Allocate struct
+    mus_t *mus = malloc(sizeof(mus_t));
+
+    // Read MUS info header
+    FILE *fp = fopen(file, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Could not open file: %s\n", file);
+        return NULL;
+    }
+    if (fread(mus, sizeof(mus_t)-sizeof(uint16_t*)-sizeof(void*), 1, fp) != 1) {
+        fprintf(stderr, "Could not read MUS header\n");
+        return NULL;
+    }
+    if (strncmp(mus->sig, "MUS", 3) != 0 || mus->sig[3] != 0x1a) {
+        fprintf(stderr, "Not a valid MUS file!\n");
+        return NULL;
+    }
+
+    // Allocate memory for all data
+    mus->instr = malloc(sizeof(uint16_t)*mus->num_instr);
+    mus->data = malloc(mus->size);
+    if (!mus->instr || !mus->data) {
+        fprintf(stderr, "Could not allocate memory\n");
+        goto error;
+    }
+
+    // Read instruments
+    if (fread(mus->instr, sizeof(uint16_t)*mus->num_instr, 1, fp) != 1) {
+        fprintf(stderr, "Could not read in instruments\n");
+        goto error;
+    }
+
+    // Read MUS data
+    if (fseek(fp, mus->offset, SEEK_SET)) {
+        fprintf(stderr, "Seeking to offset failed: %d\n", mus->offset);
+        goto error;
+    }
+    if (fread(mus->data, mus->size, 1 ,fp) != 1) {
+        fprintf(stderr, "Could not read in data");
+        goto error;
+    }
+
+    // All done!
+    fclose(fp);
+    return mus;
+
+error:
+    free(mus->instr);
+    free(mus->data);
+    free(mus);
+    mus = NULL;
+    return NULL;
+}
+
+void mus_free(mus_t *mus) {
+    if (mus != NULL) {
+        free(mus->instr);
+        free(mus->data);
+    }
+    free(mus);
+    mus = NULL;
+};
 
 int main() {
-    FILE *fp = fopen("D_E1M1.MUS", "r");
-    musheader_t musheader;
-    if (fread(&musheader, sizeof(musheader_t)-sizeof(uint16_t*)-sizeof(void*), 1, fp) == 1) {
-        printf("len: %d\n", musheader.len);
-        printf("offset: %d\n", musheader.offset);
-        printf("instruments: %d\n", musheader.numinstruments);
-    }
-
-    uint16_t instruments[musheader.numinstruments];
-    if (fread(instruments, sizeof(uint16_t)*musheader.numinstruments, 1, fp) == 1) {
-        printf("Read instruments patches\n");
-    }
-    fseek(fp, 0, SEEK_END);
-    long size = ftell(fp)-musheader.offset;
-    void *data = (void*)malloc(size);
-    if (data != NULL) {
-        printf("Allocating size of: %ld\n", size);
-    }
-
-    if (fseek(fp, musheader.offset, SEEK_SET)) {
-        printf("Seeking to offset failed: %d\n", musheader.offset);
-    } else {
-        printf("Seeking to offset: %d\n", musheader.offset);
-    }
-
-    if (fread(data, size, 1 ,fp) == 1) {
-        printf("Read data\n");
-    }
-    printf("Last byte in data: %c\n", ((char*)data)[size-1]);
-    fclose(fp);
-    printf("Done\n");
+    mus_t *mus = mus_load("D_E1M1.mus");
+    mus_free(mus);
     return 0;
 }
